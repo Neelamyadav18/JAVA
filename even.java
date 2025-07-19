6f7a2a6a650a742afac2a2068e3b668ed6fa843e
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 15:37:27 +0530

    even

---
+++ even.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        sc.close();
+        if(a%2==0){
+         System.out.print("print Even number ");
+        }
+        else{
+            System.out.print("print odd number");
+        }
+        
+
+        }
+
+    }
+    
+
+
