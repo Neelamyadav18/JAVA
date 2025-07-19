6b941b116eae17a1fa4936ab377cb4454b220794
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 22:21:03 +0530

    square

---
+++ squrt.java
@@ -0,0 +1,20 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.*;
+
+public class squrt {
+   public squrt() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the value  : ");
+      
+      int var1 = sc. nextInt();
+      System.out.print(" Square root of : "  );
+      int var2 = sc.nextInt();
+      System.out.print(" Square root of  : " );
+      
+      int var3 = var1 * var2;
+      System.out.print(" " + var3);
+   }
+}
