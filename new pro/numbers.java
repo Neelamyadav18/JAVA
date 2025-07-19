65cc0aae919813d5fc2213d284e785b8fb56fa5a
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-20 00:29:48 +0530

    1 to 10 print

---
+++ new pro/numbers.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+public class numbers {
+    public static void main(String[] args) {
+        System.out.println("enter the number :");
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+    
+        while (i<10) {
+            i = i+1;
+            System.out.println(i);
+         }
+         
+
+
+
+    }
+}
