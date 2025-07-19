75df12273c600ec26138c2029adbc8e31fdbb292
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 15:12:28 +0530

    even or odd

---
+++ condition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String[] args) {
+        System.out.print("enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for ( a = 1; a<=50 ; a++){
+
+            if (a%2==0){
+                System.out.print("  " +a);
+            } 
+                
+            }
+        }
+        
+         
+    
+}
