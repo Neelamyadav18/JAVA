ec35c84715362cdf01ae336139146f019ebc9b07
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 16:29:11 +0530

    si

---
+++ si.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class si {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the pricinple  = ");
+        float p= sc. nextFloat();
+        System.out.print("Enter the rate = ");
+        float r = sc.nextFloat();
+        System.out.print("Enter the time value = ");
+        float t = sc.nextFloat();
+        sc.close();
+       float si = (p*r*t)/100;
+        System.out.print("si= c" + si);
+
+
+    }
+
+        
+    }
+    
+
