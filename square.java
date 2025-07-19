93340b0925e50e309660420263b440371ce694bf
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 16:30:24 +0530

    square

---
+++ square.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class square {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value a = ");
+        int a = sc.nextInt();
+        int square = (a*a);
+        sc.close();
+        System.out.print("print square value = " + square);
+
+
+
+    }
+    
+}
