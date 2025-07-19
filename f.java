c042f5b5747e5e001a03a6bdfe19d9bab0346c87
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 15:58:35 +0530

    print number 1 to 10

--- f.java
+++ f.java
@@ -1,16 +1,18 @@
+
+
+import java.util.*;
 public class f {
      public static void main(String[] args){
-        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of num : ");
+        int i = sc.nextInt();
         do{
-            System.out.println(i);
+            System.out.println("num :" +i);
             i=i+1;
         }
         while (i<11); 
     
-            
-        
-
-    }
+        }
     
 }
 
