c16deb43725a9129b612a64f3cb94b05067590cc
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 16:28:12 +0530

    recatanal

---
+++ rectanal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class rectanal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float b,h;
+        System.out.print("Enter the value = ");
+        b= sc.nextFloat();
+        System.out.print("Enter the second value = ");
+        h = sc.nextFloat();
+        sc.close();
+        float react = (b*h)/2;
+        System.out.println("Cal the value react = " + react);
+
+        
+    }
+    
+}
