e7fdc3f01d8532ce623cff6bda54c9719340c75a
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 22:26:51 +0530

    sum of inte

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=0; i<=n; i++){
+            sum = sum + i;
+        System.out.println("Sum is: " + sum);
+        sc.close();
+    }
+    }
+}
