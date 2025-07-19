4d38b3f35d3998c5ee1b7b06d2f941145c9ad5d0
Neelamyadav18 <neelamyadav596@gmail.com>
2025-07-19 15:22:44 +0530

    if and else

---
+++ conditional.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        if( a<=5){
+        System.out.print("child");
+        }
+        else{
+
+System.out.println("minor ");
+}
+    }
+
+    
+}
